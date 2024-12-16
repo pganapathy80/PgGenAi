8cdff57e8431f894651ead685c5fcc0e1d05375f
pganapathy80 <pganapathy@gmail.com>
2024-12-15 17:24:20 -0800

    setup environment
    Setup environment  with AWS, python and vscode and did a test

---
+++ code/test.py
@@ -0,0 +1,8 @@
+import boto3
+session = boto3.Session(profile_name='796202783574_PGAdministratorAccess')
+bedrock = boto3.client(
+    service_name="bedrock" ,
+    region_name="us-east-1" ,
+    
+)
+print(bedrock.list_foundation_models())
